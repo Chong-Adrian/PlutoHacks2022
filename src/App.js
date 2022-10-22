3281f6e95705d5006acbb88fe77be46b013bc64a
Chong-Adrian <agchong3@gmail.com>
2022-10-21 23:03:18 -0400

    headerbar fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import HeaderBar from './components/HeaderBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Array Destructuring Example
+// const [firstCity, secondCity, thirdCity] = [
+//   "Tokyo", 
+//   "Tahoe City",
+//   "Bend"
+// ];
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <HeaderBar />
+   );}
 
 export default App;
